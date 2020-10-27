a68d3164495f1f6487c4bc60d6c642c8743ff3d4
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-10-27 08:52:07 -0700

    questions are added in js file

---
+++ script.js
@@ -0,0 +1,54 @@
+var questions = [
+    {
+        question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+        choices: ["Function", "Declaration", "String", "Variable"],
+        correctAnswer: "Function"
+    },
+    {
+        question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        choices: ["Boolean", "RegExp", "Condition", "Event"],
+        correctAnswer: "Boolean"
+    },
+    {
+        question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        choices: ["Repeater", "Loop", "Debugger", "Clone"],
+        correctAnswer: "Loop"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        correctAnswer: "console log"
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: "parentheses"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        correctAnswer: "console log"
+    },
+
+
+];
